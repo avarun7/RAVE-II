c8ebebf209d8d9d2922c0f85e64c16f8e8d1f133
e-turcotte <eturcotte@utexas.edu>
2024-10-18 16:16:01 -0500

    Add TOP.v skeleton

--- rtl/TOP.v
+++ rtl/TOP.v
@@ -1,6 +1,6 @@
 module TOP();
 
-    localparam CYCLE_TIME = 15;
+    localparam CYCLE_TIME = 5.0;
     reg clk;
 
     initial begin
@@ -8,10 +8,36 @@ module TOP();
         forever #(CYCLE_TIME / 2.0) clk = ~clk;
     end
     
-    //instantiation of the modules: front end, mapper, OOO engine, and ROB
+    //instantiation of the modules: frontend, mapper, OOOengine, regfile, ROB, and L2$
 
-    FRONT_END front_end(
+    frontend_TOP frontend(
         .clk(clk),
+        //TODO: add more inputs/outputs
+    );
+
+    mapper_TOP mapper(
+        .clk(clk),
+        //TODO: add more inputs/outputs
+    );
+
+    regfile_TOP regfile(
+        .clk(clk),
+        //TODO: add more inputs/outputs
+    );
+
+    ooo_engine_TOP ooo_engine(
+        .clk(clk),
+        //TODO: add more inputs/outputs
+    );
+
+    rob_TOP rob(
+        .clk(clk),
+        //TODO: add more inputs/outputs
+    );
+
+    l2cache_TOP l2cache(
+        .clk(clk),
+        //TODO: add more inputs/outputs
     );
 
 endmodule
