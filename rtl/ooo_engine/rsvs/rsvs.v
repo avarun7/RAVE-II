83d4fd67f0c1fd1db683033245946c01fec0f3ee
Felipe <felipejofre2001@gmail.com>
2025-03-03 00:10:26 -0600

    Ring for RSVs done,need testing

--- rtl/ooo_engine/rsvs/rsvs.v
+++ rtl/ooo_engine/rsvs/rsvs.v
@@ -1,14 +1,7 @@
+// https://electronics.stackexchange.com/questions/266587/finding-an-empty-slot-in-an-array-of-registers
+// Use of pencoders for access
+
 module rsv_entry #(parameter XLEN = 32)(
-
-);
-
-endmodule
-
-
-module rsv #(parameter XLEN=32, SIZE=16)( // Assume mapper handles SEXT
-    input clk, rst, valid_in,
-
-    // Organized as such within the RSV
     input[4:0]              rs1_reg,
     input                   rs1_received,
     input[XLEN-1:0]         rs1_value,
@@ -20,11 +13,6 @@ module rsv #(parameter XLEN=32, SIZE=16)( // Assume mapper handles SEXT
     input                   rs2_received,
     input[4:0]              rs2_reg,
 
-    /*   Update Vars, from ring from ROB   */
-    input                   update,
-    input[4:0]              update_reg,
-    input[XLEN-1:0]         update_val,
-    
     output reg[XLEN-1:0]    rs1,
     output reg[XLEN-1:0]    rs2,
     output reg[XLEN-1:0]    pc,
@@ -32,13 +20,44 @@ module rsv #(parameter XLEN=32, SIZE=16)( // Assume mapper handles SEXT
     input[2:0]              opcode_type,
     input                   additional_info
 
-        
 );
 
-reg [(3*XLEN + 19)-1:0]     reservation_station [0:SIZE-1]; // 32-bit PC, 2 32-bit values, 2 5-bit reg ids, 7 bits of opcode data, 
-reg [SIZE-1:0]              free_list;
+endmodule
+
+
+module rsv #(parameter XLEN=32, SIZE=16, PHYS_REG_SIZE=256)( // Assume mapper handles SEXT
+    input clk, rst, valid_in,
+
+    // Organized as such within the RSV
+    input[$clog2(PHYS_REG_SIZE)-1:0]    rs1_reg,
+    input                               rs1_received,
+    input[XLEN-1:0]                     rs1_value,
+    input[XLEN-1:0]                     pc_in,
+    input[4:0]                          opcode_in,
+    input[2:0]                          opcode_type_in,
+    input                               additional_info_in,
+    input[XLEN-1:0]                     rs2_value,
+    input                               rs2_received,
+    input[$clog2(PHYS_REG_SIZE)-1:0]    rs2_reg,
+
+    /*   Update Vars, from ring from ROB   */
+    input                               update,
+    input[$clog2(PHYS_REG_SIZE)-1:0]    update_reg,
+    input[XLEN-1:0]                     update_val,
+    
+    output reg[XLEN-1:0]                rs1,
+    output reg[XLEN-1:0]                rs2,
+    output reg[XLEN-1:0]                pc,
+    input[4:0]                          opcode,
+    input[2:0]                          opcode_type,
+    input                               additional_info
+
+);
+
 
 always @(posedge clk or posedge rst) begin
+
+    // 2 pointers with read and write
     
 end
 
